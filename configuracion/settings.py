42b85091537ed181ffc5f7c46e61339510e94075
unknown <jersainmarinjuarez@gmail.com>
2026-04-26 14:52:32 -0600

    feat(settings): of SGPC_Backend

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -87,17 +87,8 @@ WSGI_APPLICATION = 'configuracion.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {'default': db_from_env}
 
 
 # Password validation
@@ -175,13 +166,7 @@ env = environ.Env()
 # Esto cargará tanto las DB_ como las SUPABASE_
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+
 
 # Tus nuevas constantes de Supabase
 SUPABASE_URL = env("SUPABASE_URL")
