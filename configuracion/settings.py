8ba9bc22cad8c257590bc0930b25a748921ec287
unknown <jersainmarinjuarez@gmail.com>
2026-04-26 17:10:50 -0600

    feat(settings, netlify2): of SGPC_Backend

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -141,7 +141,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
-    "https://panteoncomunidad.netlify.app/"
+    "https://panteoncomunidad.netlify.app"
 ]
 
 CORS_EXPOSE_HEADERS = ['Content-Disposition']
